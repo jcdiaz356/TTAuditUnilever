506cce875c056ab65d8fe4d797f1ce6ec463c723
Jaime <jcdiaz356@hotmasil.com>
2018-03-02 10:59:01 -0500

    Initial commit

---
+++ app/src/main/java/com/dataservicios/ttauditunilever/util/GlobalConstant.java
@@ -0,0 +1,17 @@
+package com.dataservicios.ttauditunilever.util;
+/**
+ * Created by usuario on 11/11/2014.
+ */
+public final class GlobalConstant {
+
+    public static String dominio = "http://ttaudit.com";
+    public static final String URL_USER_IMAGES = dominio + "/media/users/";
+    public static String directory_images = "/Pictures/" ;
+    public static final String JPEG_FILE_PREFIX = "_unilever_";
+    public static final String JPEG_FILE_SUFFIX = ".jpg";
+    public static final String ALBUN_NAME = "unilever_Photo";
+    public static final String ALBUN_NAME_TEMP = "unilever_Photo_Temp";
+    public static final String ALBUN_NAME_BACKUP = "unilever_Photo_Backup";
+
+}
+
